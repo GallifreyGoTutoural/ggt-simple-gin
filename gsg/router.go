8a1480b5047acc482c0f1e9a7e1d71cb08816eab
gallifrey <956465331@qq.com>
2023-07-18 19:00:40 +0800

    feat(router): update router's struct and functions
    - add new member: root
    - add new functions: matchChild, matchChildren, insert, search
    - update functions: addRoute, getRoute

--- gsg/router.go
+++ gsg/router.go
@@ -1,21 +1,26 @@
 package gsg
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // HandleFunc defines the request handler used by gsg
 type router struct {
+	root     map[string]*node
 	handlers map[string]HandleFunc
 }
 
-// newRouter is the constructor of router
+/*
+roots key eg, roots['GET'] roots['POST']
+handlers key eg, handlers['GET-/p/:lang/doc'] handlers['POST-/p/book']
+newRouter is the constructor of router
+*/
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandleFunc)}
-}
-
-// addRoute adds a route and its handler to the router
-func (r *router) addRoute(method string, path string, handler HandleFunc) {
-	key := method + "-" + path
-	r.handlers[key] = handler
+	return &router{
+		root:     map[string]*node{},
+		handlers: make(map[string]HandleFunc),
+	}
 }
 
 func (r *router) handle(c *Context) {
@@ -25,4 +30,62 @@ func (r *router) handle(c *Context) {
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
+}
+
+func parseFullPath(fullPath string) []string {
+	vs := strings.Split(fullPath, "/")
+	paths := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			paths = append(paths, item)
+			// Only one wildcard(*) is allowed in a path
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return paths
+}
+
+// addRoute adds a route and its handler to the router
+func (r *router) addRoute(method string, fullPath string, handler HandleFunc) {
+	paths := parseFullPath(fullPath)
+	key := method + "-" + fullPath
+	_, ok := r.root[method]
+	if !ok {
+		r.root[method] = &node{}
+	}
+	r.root[method].insert(fullPath, paths, 0)
+	r.handlers[key] = handler
+}
+
+/*
+getRoute eg:
+1 normal: fullPath=[/p/go/doc] math [/p/:lang/doc]  => {"lang": "go"}
+2 [/]: fullPath=[/static/css/gallifrey.css] math [/static/*filepath]  => {"filepath": "css/gallifrey.css"}
+3 [*]: fullPath=[/p/go] math [/p/:lang] => {"lang": "go"}
+*/
+// getRoute finds the matching route according to the method and path and returns a map of parameters
+func (r *router) getRoute(method string, fullPath string) (*node, map[string]string) {
+	searchPaths := parseFullPath(fullPath)
+	params := make(map[string]string)
+	root, ok := r.root[method]
+	if !ok {
+		return nil, nil
+	}
+	n := root.search(searchPaths, 0)
+	if n != nil {
+		paths := parseFullPath(n.fullPath)
+		for index, path := range paths {
+			if path[0] == ':' {
+				params[path[1:]] = searchPaths[index]
+			}
+			if path[0] == '*' && len(path) > 1 {
+				params[path[1:]] = strings.Join(searchPaths[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
 }
