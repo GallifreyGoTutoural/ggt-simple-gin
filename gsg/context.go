dfcbaacacc1569376a2a2db564814557f07bb77a
gallifrey <956465331@qq.com>
2023-07-18 15:00:02 +0800

    feat: encapsulate Context struct and add some functions
    - add a constructor of Context
    - provide Query and PostForm func to get some Request values
    - provide simple way to quickly build String/Data/JSON/HTML Response

---
+++ gsg/context.go
@@ -0,0 +1,83 @@
+package gsg
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// H is a shortcut for map[string]interface{}
+type H map[string]interface{}
+
+// Context is the encapsulation of http.ResponseWriter and *http.Request
+type Context struct {
+	// origin objects
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// request info
+	Path   string
+	Method string
+	// response info
+	StatusCode int
+}
+
+// newContext is the constructor of Context
+func newContext(w http.ResponseWriter, r *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    r,
+		Path:   r.URL.Path,
+		Method: r.Method,
+	}
+}
+
+// PostForm is a wrapper of r.PostFormValue()
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+// Query is a wrapper of r.URL.Query().Get()
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+// Status sets the status code for response
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+// SetHeader sets the header for response
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+// String sets the string for response
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+// JSON sets the json for response
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+// Data sets the data for response
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+// HTML sets the html for response
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
